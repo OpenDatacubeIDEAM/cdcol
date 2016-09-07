95ea8df5af0915e1563e4322cafc90d1f9f20add
Christian Ariza <christian.ariza@gmail.com>
2016-09-06 19:40:20 -0500

    Archivos de configuración de ingesta

--- configIngester/IngetFolderUSGS.sh
+++ configIngester/IngetFolderUSGS.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Script para ingestar un conjunto de imágenes USGS (landsat 7 ledaps) realizando los procesos previos (descomprimir, preparar, indexar)
 #El primer parámetro es la carpeta en la que se encuentran los tar.gz del USGS, si se llama sin parámetros toma la carpeta actual
+#El segundo parámetro es el archivo de configuración de ingesta, por defecto usará el de landsat 7 wgs84
 basePath="${1:-.}" 
-cd basePath
+configFile="${2:-~/configIngester/ls7_ledaps_wgs84.yaml}"
+cd $basePath
 for archivo in *.tar.gz
 do
 folder="tmp/${archivo%%.*}"
@@ -10,5 +12,5 @@ base=${archivo%%-*}
 mkdir -p $folder  && tar -xzf $archivo -C $folder
 python ~/agdc-v2/utils/usgslsprepare.py $folder && datacube dataset add -a $folder
 done
-datacube ingest  -c ~/configIngester/ls7_ledaps_wgs84.yaml
+datacube ingest  -c $configFile
 rm -rf tmp
