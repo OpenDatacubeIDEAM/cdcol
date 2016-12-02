842fb2730226d8022e4f9fe64aebb16eac52d83d
Christian Fernando Ariza Porras <cf.ariza975@uniandes.edu.co>
2016-12-02 16:38:42 -0500

    Generar los thumbnails con colores

--- util/generateThumbnails.sh
+++ util/generateThumbnails.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Parámetros (opcionales) Unidad de almacenamiento, resolución. 
+export GDAL_DATA="${GDAL_DATA:-/usr/share/gdal/1.11}"
 STORAGE_UNIT="${1:-LS7_ETM_LEDAPS}" 
 TN_FOLDER=/web_storage/thumbnails
 RES="${2:-500}"
+CLU='panoply'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $TN_FOLDER/$STORAGE_UNIT
 for file in /dc_storage/$STORAGE_UNIT/*.nc
 do
@@ -10,6 +13,9 @@ for ds in `gdalinfo $file |grep -Eo "NETCDF.*"`
 do
 bn=`basename $file`
 echo "Escribiendo los thumbnails para el archivo $file y la banda ${ds##*\:}"
-gdal_translate  -a_srs EPSG:32618 -a_nodata -9999 -stats -of PNG -scale -outsize $RES $RES $ds $TN_FOLDER/$STORAGE_UNIT/${bn%.nc}.${ds##*\:}.png
+OFPNG=$TN_FOLDER/$STORAGE_UNIT/${bn%.nc}.${ds##*\:}.png
+(gdal_translate -q  -a_srs EPSG:32618 -stats -of PNG -scale -ot Byte -outsize $RES $RES $ds $OFPNG  2>~/errors|| (rm $OFPNG && false)) && \
+convert -transparent "#000000" $OFPNG $DIR/colores/$CLU.png -clut $OFPNG &&\
+convert $OFPNG   -background Khaki  label:"${bn%.nc}.${ds##*\:}" -gravity Center -append  $OFPNG &
 done
 done
--- util/generateThumbnailsResultadosColorGif.sh
+++ util/generateThumbnailsResultadosColorGif.sh
@@ -11,6 +11,7 @@ echo $GDAL_DATA
 FOLDER="${1:-.}"
 TN_FOLDER="$FOLDER/thumbnails"
 RES="${2:-500}"
+CLU='panoply'
 mkdir -p $TN_FOLDER
 for file in $FOLDER/*.nc
 do
@@ -21,7 +22,8 @@ then
 	do
 	echo "Escribiendo los thumbnails para el archivo $file y la banda ${ds##*\:}"
 	gdal_translate  -a_srs EPSG:32618   -stats -of PNG -scale -ot Byte -outsize $RES $RES $ds $TN_FOLDER/${bn%.nc}.${ds##*\:}.png
-	convert -transparent "#000000" $TN_FOLDER/${bn%.nc}.${ds##*\:}.png $DIR/colores/rainbow1.png -clut $TN_FOLDER/${bn%.nc}.${ds##*\:}.png
+	convert -transparent "#000000" $TN_FOLDER/${bn%.nc}.${ds##*\:}.png $DIR/colores/$CLU.png -clut $TN_FOLDER/${bn%.nc}.${ds##*\:}.png
+	convert $TN_FOLDER/${bn%.nc}.${ds##*\:}.png   -background Khaki  label:"${bn%.nc}.${ds##*\:}" -gravity Center -append  $TN_FOLDER/${bn%.nc}.${ds##*\:}.png
 	done
 else
 	
@@ -31,17 +33,19 @@ else
 	then
 		echo "Escribiendo el thumbnail para el archivo $file"
 		gdal_translate  -a_srs EPSG:32618  -stats -of PNG -scale -ot Byte -outsize $RES $RES $file $TN_FOLDER/${bn%.nc}.png
-		convert -transparent "#000000" $TN_FOLDER/${bn%.nc}.png $DIR/colores/rainbow1.png -clut $TN_FOLDER/${bn%.nc}.png
+		convert -transparent "#000000" $TN_FOLDER/${bn%.nc}.png $DIR/colores/$CLU.png -clut $TN_FOLDER/${bn%.nc}.png
+		convert $TN_FOLDER/${bn%.nc}.png   -background Khaki  label:"${bn%.nc}" -gravity Center -append  $TN_FOLDER/${bn%.nc}.png
 	else
 		for i in $(seq 1 $nb)
 		do
 		echo "Escribiendo el thumbnail para el archivo $file banda $i"
-
-		gdal_translate  -a_srs EPSG:32618  -stats -of PNG -scale -ot Byte -b $i -outsize $RES $RES $file $TN_FOLDER/${bn%.nc}.$i.png
-		convert -transparent "#000000" $TN_FOLDER/${bn%.nc}.$i.png $DIR/colores/rainbow1.png -clut $TN_FOLDER/${bn%.nc}.$i.png
+		n=`printf %05d $i`
+		(gdal_translate  -a_srs EPSG:32618  -stats -of PNG -scale -ot Byte -b $i -outsize $RES $RES $file $TN_FOLDER/${bn%.nc}.$n.png || (rm $TN_FOLDER/${bn%.nc}.$n.png && false)) &&\
+		convert -transparent "#000000" $TN_FOLDER/${bn%.nc}.$n.png $DIR/colores/$CLU.png -clut $TN_FOLDER/${bn%.nc}.$n.png && \
+		convert -quiet $TN_FOLDER/${bn%.nc}.$n.png   -background Khaki  label:"${bn%.nc}.$i" -gravity Center -append  $TN_FOLDER/${bn%.nc}.$n.png 
 		done
 	fi
 fi
 done
 
-convert -dispose previous -delay 20 -loop 0 -coalesce   $TN_FOLDER/*.png $TN_FOLDER/animated.gif
+convert -dispose background -delay 100 -loop 0 -coalesce   $TN_FOLDER/*.png $TN_FOLDER/animated.gif
