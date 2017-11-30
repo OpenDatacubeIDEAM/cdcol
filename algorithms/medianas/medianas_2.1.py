ab9249c3178d4039df283fd625739d6d967f25d8
Christian Fernando Ariza Porras <cf.ariza975@uniandes.edu.co>
2017-11-29 21:37:10 -0500

    Cambios para soportar máscaras dependiendo del sensor.

--- algorithms/medianas/medianas_2.1.py
+++ algorithms/medianas/medianas_2.1.py
@@ -1,16 +1,22 @@
 import xarray as xr
 import numpy as np
 print "Excecuting medianas v2"
+def isin(element, test_elements, assume_unique=False, invert=False):
+    "definiendo la función isin de numpy para la versión anterior a la 1.13, en la que no existe"
+    element = np.asarray(element)
+    return np.in1d(element, test_elements, assume_unique=assume_unique,
+                invert=invert).reshape(element.shape)
 nbar = xarr0
 nodata=-9999
 medians={}
+
 validValues=set()
 if product=="LS7_ETM_LEDAPS":
-    validValues=set(66,68,130,132)
+    validValues=[66,68,130,132]
 elif product == "LS8_OLI_LASRC":
-    validValues=set(322, 386, 834, 898, 1346, 324, 388, 836, 900, 1348)
+    validValues=[322, 386, 834, 898, 1346, 324, 388, 836, 900, 1348]
 
-cloud_mask=np.where(nbar["pixel_qa"].values in validValues, True, False)
+cloud_mask=isin(nbar["pixel_qa"].values, validValues)
 for band in bands:
     datos=np.where(np.logical_and(nbar.data_vars[band]!=nodata,cloud_mask),nbar.data_vars[band], np.nan)
     allNan=~np.isnan(datos)
