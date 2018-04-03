9f65c2384b15b5be7c2eeb2b13f1851d4ad63a06
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-04-03 08:42:37 -0500

    Comentando kmeans

--- algorithms/clasificacion-no-supervisada-k-means/clasificacion-no-supervisada-k-means_1.1.py
+++ algorithms/clasificacion-no-supervisada-k-means/clasificacion-no-supervisada-k-means_1.1.py
@@ -4,6 +4,9 @@ from scipy.cluster.vq import kmeans2,vq
 import xarray as xr
 import numpy as np
 
+
+#La funcion isin permite verificar si el elemento (elemento) que se pasa por parametro está dentro del arreglo (test_elements)
+#Esta funcion se define para verificar los valores validos de la mascara de nube (pixel_qa) 
 def isin(element, test_elements, assume_unique=False, invert=False):
     "definiendo la función isin de numpy para la versión anterior a la 1.13, en la que no existe"
     element = np.asarray(element)
@@ -11,24 +14,38 @@ def isin(element, test_elements, assume_unique=False, invert=False):
 
 #Calcular el compuesto de medianas para cada uno de las entradas
 nbar = xarr0
+#Un valor negativo suficientemente grande para representar cuando no hay datos
 nodata=-9999
 medians1={}
+#Conjunto de valores valido para la mascara de nube
 validValues=set()
+
+#Dependiendo del satellite, se definen unos valores distintos para la mascara
+#Estos valores se definen en la página de LandSat: https://landsat.usgs.gov/landsat-surface-reflectance-quality-assessment
+#Son valores validos porque representan agua (water) o un "pixel claro" (clear)
 if product=="LS7_ETM_LEDAPS":
     validValues=[66,68,130,132]
 elif product == "LS8_OLI_LASRC":
     validValues=[322, 386, 834, 898, 1346, 324, 388, 836, 900, 1348]
 
 cloud_mask=isin(nbar["pixel_qa"].values, validValues)
+
+#Se hace un recorrido sobre las bandas
 for band in bands:
+    # np.where es la funcion where de la libreria numpy que retorna un arreglo o un set de arreglos con datos que cumplen con la condicion dada por parametro
+    #
     datos=np.where(np.logical_and(nbar.data_vars[band]!=nodata,cloud_mask),nbar.data_vars[band], np.nan)
     allNan=~np.isnan(datos)
     if normalized:
+        #Calcula la mediana arimetica sobre el arreglo de datos  ,sobre el eje  , ignorando los valores NaN
         m=np.nanmean(datos.reshape((datos.shape[0],-1)), axis=1)
+        #Calcula la desviacion estandar sobre el arreglo de datos  , sobre el eje , ignorando los valores NaN
         st=np.nanstd(datos.reshape((datos.shape[0],-1)), axis=1)
         datos=np.true_divide((datos-m[:,np.newaxis,np.newaxis]), st[:,np.newaxis,np.newaxis])*np.nanmean(st)+np.nanmean(m)
+    #Calcula la mediana aritmetica 
     medians1[band]=np.nanmedian(datos,0)
     medians1[band][np.sum(allNan,0)<minValid]=np.nan
+#Elimina la variable datos y la asociacion que tiene en el algoritmo
 del datos
 
 #Preprocesar:
@@ -66,6 +83,8 @@ for coordenada in xarr0.coords:
         dimensiones.append(coordenada)
         xcords[coordenada] = xarr0.coords[coordenada]
 valores = {"kmeans": xr.DataArray(kmv, dims=dimensiones, coords=coordenadas)}
+#Genera el dataset (netcdf) con las bandas con el sistema de referencia de coordenadas
 output = xr.Dataset(valores, attrs={'crs': xarr0.crs})
+
 for coordenada in output.coords:
     output.coords[coordenada].attrs["units"] = xarr0.coords[coordenada].units
